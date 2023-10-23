141a60299188817e288bb722efab924cda1836bd
Sterling Smith <sterling12311@gmail.com>
2023-10-23 14:11:05 -0600

    rendered image cards to the page

--- client.js
+++ client.js
@@ -11,7 +11,7 @@ for (let i = 0; i < 5; i++) {
 document.getElementById("right-triangle").innerHTML = rightTriangle;
 
 // Full Pyramid
-let n = 10;
+let n = 25;
 let fullPyramid = "";
 
 for (let i = 1; i <= n; i++) {
@@ -24,5 +24,35 @@ for (let i = 1; i <= n; i++) {
   fullPyramid += "<br>";
 }
 
-document.getElementById("full-pyramid").innerHTML = fullPyramid
+document.getElementById("full-pyramid").innerHTML = fullPyramid;
 
+// Reverse a String
+const cards = [
+  {title: "Card A", content: "This is the first image card"},
+  {title: "Card B", content: "This is the second image card"},
+  {title: "Card C", content: "This is the third image card"},
+];
+
+const cardContainer = document.getElementById("card-container");
+
+  // Render cards
+  cards.forEach(card => {
+    // Create a card element
+    const cardElement = document.createElement("div");
+    cardElement.classList.add("card");
+
+    // Set card title
+    const cardTitle = document.createElement("h2");
+    cardTitle.textContent = card.title;
+
+    // Set card content
+    const cardContent = document.createElement("p");
+    cardContent.textContent = card.content;
+
+    // Append title and content to the card element
+    cardElement.appendChild(cardTitle);
+    cardElement.appendChild(cardContent);
+
+    // Append the card element to the card container
+    cardContainer.appendChild(cardElement);
+});
